3ac71993bd35856f712922d89ad14c4f06978f2f
Blake Ellingham <blake.ellingham@rocksaucestudios.com>
2013-11-27 02:42:13 -0600

    Added image files

---
+++ RecommenuBeta/RecommenuBeta/RMUHomeScreen.h
@@ -0,0 +1,14 @@
+//
+//  RMUHomeScreen.h
+//  RecommenuBeta
+//
+//  Created by Blake Ellingham on 11/23/13.
+//  Copyright (c) 2013 Blake Ellingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MapBox/MapBox.h"
+
+@interface RMUHomeScreen : UIViewController
+
+@end
---
+++ RecommenuBeta/RecommenuBeta/RMUPointAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  RMUPointAnnotation.h
+//  RecommenuBeta
+//
+//  Created by Blake Ellingham on 11/24/13.
+//  Copyright (c) 2013 Blake Ellingham. All rights reserved.
+//
+
+#import <MapBox/MapBox.h>
+
+@interface RMUPointAnnotation : RMPointAnnotation
+
+@end
